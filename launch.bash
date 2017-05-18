e0413ec62b8c6b23b726a994eec0f3de90f967b7
L-Cafe <L-Cafe@users.noreply.github.com>
2017-05-19 01:50:47 +0200

    fix launch for non PWD

--- launch.bash
+++ launch.bash
@@ -5,4 +5,7 @@ export routerAddress="<YOUR ROUTER ADDRESS>"
 export routerUsername="<YOUR ROUTER USERNAME>"
 export routerPassword="<YOUR ROUTER PASSWORD>"
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $DIR
+
 `which python3` python/check.py
