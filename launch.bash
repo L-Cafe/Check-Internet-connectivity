727fa50298278b196b0f9f3f4faf1194f29693a2
L-Cafe <L-Cafe@users.noreply.github.com>
2017-05-19 01:44:01 +0200

    added launch file with creds

---
+++ launch.bash
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Router credentials
+export routerAddress="<YOUR ROUTER ADDRESS>"
+export routerUsername="<YOUR ROUTER USERNAME>"
+export routerPassword="<YOUR ROUTER PASSWORD>"
+
+`which python3` python/check.py
